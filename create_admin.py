dd3dae4f22adfa7f039ec8e2c024641ce50560c1
oussema ghanmi <206705974+oussemaghanmi@users.noreply.github.com>
2026-02-09 00:25:07 +0100

    Add script to create superuser if not exists

--- create_admin.py
+++ create_admin.py
@@ -1,1 +1,13 @@
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+username = "admin"
+password = "Admin@Clinic2026"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser 'admin' created.")
+else:
+    print("Superuser 'admin' already exists.")
